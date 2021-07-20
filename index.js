7d1782e6cb7d42365eda1b8aeb85cc8c0e666905
msalmasi <michael@max2.com>
2021-07-20 02:05:13 -0400

    Uploading main build.

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const express = require('express')
+const https = require('https')
+const fs = require('fs')
+const app = express()
+const port = 80
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./db.sqlite')
+const axios = require('axios')
+const STREAMLABS_API_BASE = 'https://www.streamlabs.com/api/v1.0'
+
+app.set("view engine", "ejs")
+app.use(express.static('public'))
+app.use(express.urlencoded({
+  extended: true
+}))
+
+// https
+//   .createServer(
+//     {
+//       key: fs.readFileSync('/etc/letsencrypt/live/thelounge.casino-0001/privkey.pem'),
+//       cert: fs.readFileSync('/etc/letsencrypt/live/thelounge.casino-0001/fullchain.pem'),
+//       ca: fs.readFileSync('/etc/letsencrypt/live/thelounge.casino-0001/fullchain.pem'),
+//     },
+//     app
+//   )
+//   .listen(443, () => {
+//     console.log('Listening...')
+//   })
+
+
+app.get('/', (req, res) => {
+    res.render("index");
+})
+
+app.listen(port, () => console.log(`Hashery-Web listening on port ${port}!`))
